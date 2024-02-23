802fee2cc254a0812dd626ad3029c11723a1247d
Shreejesh J Ballal <shreejeshballal@gmail.com>
2024-02-23 18:55:31 +0530

    feat: Connect to db, create user schema and add user actions

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import User from "../models/user.model";
+import { connectToDB } from "../mongoose";
+
+interface Params {
+  userId: string;
+  username: string;
+  bio: string;
+  image: string;
+  name: string;
+  path: string;
+}
+
+export async function updateUser({
+  userId,
+  username,
+  bio,
+  image,
+  name,
+  path,
+}: Params): Promise<void> {
+  connectToDB();
+  try {
+    await User.findOneAndUpdate(
+      { id: userId },
+      { username: username.toLowerCase(), name, bio, image, onboarded: true },
+      {
+        upsert: true,
+      }
+    );
+
+    if (path === "/profile/edit") {
+      revalidatePath(path);
+    }
+  } catch (error: any) {
+    throw new Error(`Failed to create/update user: ${error.message}`);
+  }
+}
+
+export async function fetchUser(userId: string) {
+  try {
+    connectToDB();
+    return await User.findOne({ id: userId });
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user info: ${error.message}`);
+  }
+}
